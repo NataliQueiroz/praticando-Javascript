58e19b8b888ab1c96367083f9354e21225331359
Natali IBBI <natali.queiroz@ibbi.org.br>
2024-12-04 12:51:49 -0400

    Aprendendo variáveis

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+//Variáveis
 
+//Existem 3 tipos de variáveis: var, let e const
+// var e let são similares, sendo var a forma antiga de criar uma variável, e let, a forma após a atualização de 2015
+// O nome da variável, precisa seguir algumas regras: pode ter letras, números, _ e $. Porém não pode haver espaços nas variáveis
+// e não podem começar com números.
+
+let firstName = "Laranja";
+let lastName = "Queiroz";
+let age = 2;
+console.log(firstName, lastName, age);
