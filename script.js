03b3693d6b894600371c3d5e75f18fa25513ba38
Natali IBBI <natali.queiroz@ibbi.org.br>
2024-12-04 13:33:42 -0400

    diferenciando let de const

--- script.js
+++ script.js
@@ -4,8 +4,13 @@
 // var e let são similares, sendo var a forma antiga de criar uma variável, e let, a forma após a atualização de 2015
 // O nome da variável, precisa seguir algumas regras: pode ter letras, números, _ e $. Porém não pode haver espaços nas variáveis
 // e não podem começar com números.
+// em let, você consegue manipular os valores das variáveis. em const, não.
+// em const, se você inicializar uma variável, você precisa atribuir um valor a ela
 
 let firstName = "Laranja";
 let lastName = "Queiroz";
 let age = 2;
 console.log(firstName, lastName, age);
+
+const calcTax = 0.7;
+console.log(calcTax);
