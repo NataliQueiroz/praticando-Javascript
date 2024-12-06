c9d349b607c8af1d5f63a21d9b6343af03ce3e2b
Natali IBBI <natali.queiroz@ibbi.org.br>
2024-12-06 10:17:25 -0400

    Aprendendo sobre linguagens estáticas e dinâmicas

--- script.js
+++ script.js
@@ -1,46 +1,9 @@
-// Data Types
-// Primitive - Reference
+// Static ou Dynamic
 
-//String
-let firstName = "Luisa";
-console.log(firstName, typeof firstName);
+// Static
+// Exemplos de linguagens estáticas: C, C++, Java, TypeScript
 
-//Number
-let age = 35;
-console.log(age, typeof age);
-
-//Boolean
-let isAdult = false;
-
-//Null -  valor zero
-let address = null;
-console.log(address, typeof address);
-
-//Undefined - não é um valor
-let color;
-console.log(color, typeof color);
-
-//Symbol - representa um valor primitivo único e é imutável
-let id = Symbol("id");
-let meuObjeto = {
-  [id]: 123456,
-};
-console.log(meuObjeto[id]);
-
-// Data Types
-// Reference Data Types
-
-// Arrays - lista de itens dentro de uma variável
-
-let numbers = [10, 20, 30, 35, 50];
-console.log(numbers, typeof numbers);
-
-// Function
-function myName() {
-  console.log("Naruto");
-}
-console.log(myName, typeof myName);
-
-// Date - data
-let now = new Date();
-console.log(now);
+//Dynamic - para uma linguagem ser considerada dinâmica, ela precisa associar o tipo de objeto sem que esteja escrito o tipo.
+// Exemplos de linguagens dinâmicas: Python, Javascript, Ruby, PHP
+let firstName = "Natali"; // observe que não foi preciso dizer que era uma string, a própria linguagem associou.
+let age = 29; // o mesmo aconteceu aqui.
