1a5bc34e554b1d2db08fd6927ba30fd17aebcac1
Natali IBBI <natali.queiroz@ibbi.org.br>
2024-12-06 10:41:39 -0400

    convertendo String para Number

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-// Static ou Dynamic
+// Convertions ou Casting
 
-// Static
-// Exemplos de linguagens estáticas: C, C++, Java, TypeScript
+let age = "35.2";
+console.log(age, typeof age);
 
-//Dynamic - para uma linguagem ser considerada dinâmica, ela precisa associar o tipo de objeto sem que esteja escrito o tipo.
-// Exemplos de linguagens dinâmicas: Python, Javascript, Ruby, PHP
-let firstName = "Natali"; // observe que não foi preciso dizer que era uma string, a própria linguagem associou.
-let age = 29; // o mesmo aconteceu aqui.
+//ParseInt - é uma função que analisa uma String e retorna um número inteiro.
+
+// age = parseInt(age);
+// console.log(age, typeof age);
+
+//ParseFloat - deixa com que eu converta um número que contenha uma fração (FRACIONÁRIO)
+// age = parseFloat(age);
+// console.log(age, typeof age);
+
+// Unário (+) faz o papel do int e do float ao mesmo tempo
+// age = +age;
+// console.log(age, typeof age);
+
+//Number - converte para número (tanto inteiro, quanto float)
+age = Number(age);
+console.log(age, typeof age);
+
+// Quais os mais recomendados? Tanto o number, quanto o unário
