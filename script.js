2914bfbdabfaaf9e3a36398d4c7d11d4e6a5cc06
Natali IBBI <natali.queiroz@ibbi.org.br>
2024-12-06 10:53:58 -0400

    Convertendo de String para Number

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
 // Convertions ou Casting
 
-let age = "35.2";
+let age = 35;
 console.log(age, typeof age);
 
-//ParseInt - é uma função que analisa uma String e retorna um número inteiro.
-
-// age = parseInt(age);
-// console.log(age, typeof age);
-
-//ParseFloat - deixa com que eu converta um número que contenha uma fração (FRACIONÁRIO)
-// age = parseFloat(age);
-// console.log(age, typeof age);
-
-// Unário (+) faz o papel do int e do float ao mesmo tempo
-// age = +age;
-// console.log(age, typeof age);
-
-//Number - converte para número (tanto inteiro, quanto float)
-age = Number(age);
-console.log(age, typeof age);
-
-// Quais os mais recomendados? Tanto o number, quanto o unário
+//Number para String
+//age = age.toString() - usando o método toString
+// age = String(age); - usando o constructor
