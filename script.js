f4fb00448a703707e65adcede4f9bb8d6481f6d1
Natali IBBI <natali.queiroz@ibbi.org.br>
2024-12-06 14:03:07 -0400

    convertendo de number para boolean

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 // Convertions ou Casting
 
-let age = 35;
+let age = 30;
+age = Boolean(age);
 console.log(age, typeof age);
 
-//Number para String
+//Number para Boolean (true ou false)
+// Number 0 = false
+// Number 1... = true
+
 //age = age.toString() - usando o método toString
 // age = String(age); - usando o constructor
